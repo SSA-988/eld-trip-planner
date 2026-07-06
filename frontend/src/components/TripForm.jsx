76f6d8d9f5c7bf28442247787ca64ccab78cf3bb
Sujan Anand <sujananand@Sujans-MacBook-Pro-2.local>
2026-07-06 20:43:32 +0530

    Add production API URL

--- frontend/src/components/TripForm.jsx
+++ frontend/src/components/TripForm.jsx
@@ -1,6 +1,9 @@
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL || "http://127.0.0.1:8000";
+// const API_URL = import.meta.env.VITE_API_URL || "http://127.0.0.1:8000";
+const API_URL =
+  import.meta.env.VITE_API_URL ||
+  "https://eld-trip-planner-production-837a.up.railway.app";
 
 function TripForm({ setTripData, setLoading, setError, loading }) {
   const handleSubmit = async (e) => {
